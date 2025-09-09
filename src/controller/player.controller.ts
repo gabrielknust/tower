7fade302f3c37bc7e3478ef2012247ae3888df4a
Gabriel Knust <gabrielsilvaknustws@gmail.com>
2025-09-09 15:43:44 -0300

    feat: improve the entrance validation and the player route unit tests

--- src/controller/player.controller.ts
+++ src/controller/player.controller.ts
@@ -6,10 +6,10 @@ import * as z from 'zod';
 export async function createPlayer(req: Request, res: Response) {
     const trx = await db.transaction();
     const fighterSchema = z.object({
-        name: z.string("O campo 'name' deve ser uma string.").min(1,"O campo 'name' é obrigatório."),
-        fighter_id: z.number().min(1, { message: "O 'fighter_id' deve ser um número." }),
-        cfn: z.string().min(1, { message: "O campo 'cfn' é obrigatório." }),
-        tower_id: z.number().min(1, { message: "O 'tower_id' deve ser um número." })
+        name: z.string("O campo 'nome' é obrigatório.").min(1, "O campo 'name' é obrigatório."),
+        fighter_id: z.number("O campo 'fighter_id' é obrigatório e deve ser um número inteiro").int("O 'fighter_id' deve ser um número inteiro"),
+        cfn: z.string("O campo 'cfn' é obrigatório.").min(1, "O campo 'cfn' é obrigatório."),
+        tower_id: z.number("O campo 'tower_id' é obrigatório e deve ser um número inteiro.").int("O 'tower_id' deve ser um número inteiro.")
     });
     try {
         fighterSchema.parse(req.body);
@@ -18,9 +18,8 @@ export async function createPlayer(req: Request, res: Response) {
         const existingPlayer = await trx('player')
             .where({ fighter_id: player.fighterId })
             .first();
-
         let result;
-        if (existingPlayer) {
+        if (existingPlayer && existingPlayer.deleted_at != null) {
             result = await trx('player')
                 .where({ fighter_id: player.fighterId })
                 .update({
@@ -28,7 +27,9 @@ export async function createPlayer(req: Request, res: Response) {
                     cfn: player.cfn,
                     deleted_at: null
                 })
-                .returning(['name', 'fighter_id', 'cfn']);
+                .returning(['name', 'fighter_id', 'cfn', 'deleted_at']);
+        } else if (existingPlayer && existingPlayer.deleted_at==null) {
+            return res.status(409).json({ error: 'Fighter ID already exists' });
         } else {
             result = await trx('player')
                 .insert({
@@ -36,9 +37,8 @@ export async function createPlayer(req: Request, res: Response) {
                     fighter_id: player.fighterId,
                     cfn: player.cfn
                 })
-                .returning(['name', 'fighter_id', 'cfn']);
+                .returning(['name', 'fighter_id', 'cfn', 'deleted_at']);
         }
-
         const insertTower = await trx('classification')
             .insert({
                 player_id: fighter_id,
@@ -60,8 +60,12 @@ export async function createPlayer(req: Request, res: Response) {
 }
 
 export async function getPlayerById(req: Request, res: Response) {
+    const paramsSchema = z.object({
+        fighter_id: z.coerce.number("O campo 'fighter_id' é obrigatório e deve ser um número inteiro").int("O 'fighter_id' deve ser um número inteiro"),
+    });
+
     try {
-        const { fighter_id } = req.params;
+        const { fighter_id } = paramsSchema.parse(req.params);
         const player = await db('player')
             .where({ fighter_id: fighter_id })
             .first();
@@ -71,6 +75,9 @@ export async function getPlayerById(req: Request, res: Response) {
             return res.status(404).json({ error: "Jogador não encontrado." });
         }
     } catch (err) {
+        if (err instanceof z.ZodError) {
+            return res.status(400).json({ errors: err });
+        }
         console.error(err);
         return res.status(500).json({ error: "Erro ao buscar jogador." });
     }   
@@ -89,15 +96,24 @@ export async function getAllPlayers(req: Request, res: Response) {
 }
 
 export async function updatePlayer(req: Request, res: Response) {
-    const { fighter_id } = req.params;
-    const { name, cfn } = req.body;
+    const paramsSchema = z.object({
+        fighter_id: z.coerce.number("O campo 'fighter_id' é obrigatório e deve ser um número inteiro").int("O 'fighter_id' deve ser um número inteiro"),
+    });
+
+    const bodySchema = z.object({
+        name: z.string().optional(),
+        cfn: z.string().optional()
+    });
+
     const trx = await db.transaction();
     try {
+        const { fighter_id } = paramsSchema.parse(req.params);
+        const { name, cfn } = bodySchema.parse(req.body);
         const updated = await trx('player')
             .where({ fighter_id: fighter_id })
             .update({ name, cfn })
             .returning(['name', 'fighter_id', 'cfn']);
-            
+
         await trx.commit();
         if (updated.length) {
             return res.status(200).json(updated[0]);
@@ -106,15 +122,21 @@ export async function updatePlayer(req: Request, res: Response) {
         }
     } catch (err) {
         await trx.rollback();
+        if (err instanceof z.ZodError) {
+            return res.status(400).json({ errors: err });
+        }
         console.error(err);
         return res.status(500).json({ error: "Erro ao atualizar jogador." });
     }
 }
 
 export async function deletePlayer(req: Request, res: Response) {
+    const paramsSchema = z.object({
+        fighter_id: z.coerce.number("O campo 'fighter_id' é obrigatório e deve ser um número inteiro").int("O 'fighter_id' deve ser um número inteiro"),
+    });
     const trx = await db.transaction();
-    const { fighter_id } = req.params;
     try {
+        const { fighter_id } = paramsSchema.parse(req.params);
         await trx('classification').where({ player_id: fighter_id }).del();
         const deleted = await trx('player').where({ fighter_id: fighter_id }).update({ deleted_at: trx.fn.now() });
         await trx.commit();
@@ -126,6 +148,9 @@ export async function deletePlayer(req: Request, res: Response) {
     } catch (err) {
         await trx.rollback();
         console.error(err);
+        if (err instanceof z.ZodError) {
+            return res.status(400).json({ errors: err });
+        }
         return res.status(500).json({ error: "Erro ao deletar jogador." });
     }
 }
--- test/router/player.routes.test.ts
+++ test/router/player.routes.test.ts
@@ -1,9 +1,19 @@
 import request from 'supertest';
 import { db } from '../../postgres/database';
 
-describe('POST /player', () => {
+describe('/player', () => {
+
+    const createdFighterIds = new Set<number>();
+
     afterAll(async () => {
         await db.destroy();
+    });
+
+    afterEach(async () => {
+        for (const fighter_id of createdFighterIds) {
+            await db('player').where('fighter_id', fighter_id).del();
+        }
+        createdFighterIds.clear();
     });
 
     it('should create a new player', async () => {
@@ -12,18 +22,30 @@ describe('POST /player', () => {
         const fighter_id = 12355533;
         const tower_id = 2;
         const body = { name, cfn, fighter_id, tower_id };
-        try {
-            const response = await request('http://localhost:8005').post('/player').send(body);
-            expect(response.status).toBe(201);
-            expect(response.body).toEqual({ name, cfn, fighter_id });
-            const count = await db('players').where('fighter_id', fighter_id).select().count();
-            expect(count[0]['count']).toBe('1');
-        } catch (error) {
-            console.error(error);
-        }
-        finally {
-            await db('player').where('fighter_id', fighter_id).del();
-        }
+        createdFighterIds.add(fighter_id);
+        const response = await request('http://localhost:8005').post('/player').send(body);
+        expect(response.status).toBe(201);
+        expect(response.body).toEqual({
+            classification: {
+                id: expect.any(Number),
+                position: expect.any(Number),
+                tower_id,
+                player_id: fighter_id.toString(),
+            },
+            player: {
+                name,
+                cfn,
+                fighter_id: fighter_id.toString(),
+                deleted_at: null
+            }
+        });
+        const player = await db('player').where('fighter_id', fighter_id).first();
+        expect(player).toMatchObject({
+            name,
+            cfn,
+            fighter_id: fighter_id.toString(),
+            deleted_at: null
+        });
     });
 
     it('should return 400 if name is missing', async () => {
@@ -56,15 +78,14 @@ describe('POST /player', () => {
             {
                 code: 'invalid_type',
                 path: ['cfn'],
-                message: expect.stringContaining("Invalid input: expected string, received undefined"),
+                message: expect.stringContaining("O campo 'cfn' é obrigatório."),
             },
         ]);
     });
 
-    it.only('should return 400 if fighter_id is missing', async () => {
+    it('should return 400 if fighter_id is missing', async () => {
         const name = 'player1';
         const cfn = "player bobao";
-        const fighter_id = 0;
         const tower_id = 2;
         const body = { name, cfn, tower_id };
         const response = await request('http://localhost:8005').post('/player').send(body);
@@ -74,7 +95,7 @@ describe('POST /player', () => {
             {
                 code: 'invalid_type',
                 path: ['fighter_id'],
-                message: expect.stringContaining("Invalid input: expected string, received undefined"),
+                message: expect.stringContaining("O campo 'fighter_id' é obrigatório e deve ser um número inteiro"),
             },
         ]);
     });
@@ -91,7 +112,79 @@ describe('POST /player', () => {
             {
                 code: 'invalid_type',
                 path: ['tower_id'],
-                message: expect.stringContaining("Invalid input: expected string, received undefined"),
+                message: expect.stringContaining("O campo 'tower_id' é obrigatório e deve ser um número inteiro"),
+            },
+        ]);
+    });
+
+    it('should return 400 if name is not a string', async () => {
+        const name = 12345;
+        const cfn = "player bobao";
+        const fighter_id = 12355533;
+        const tower_id = 2;
+        const body = { name, cfn, fighter_id, tower_id };
+        const response = await request('http://localhost:8005').post('/player').send(body);
+        const errorMessages = JSON.parse(response.body.errors.message);
+        expect(response.status).toBe(400);
+        expect(errorMessages).toMatchObject([
+            {
+                code: 'invalid_type',
+                path: ['name'],
+                message: expect.stringContaining("O campo 'nome' é obrigatório."),
+            },
+        ]);
+    });
+
+    it('should return 400 if cfn is not a string', async () => {
+        const name = 'player1';
+        const cfn = 12345;
+        const fighter_id = 12355533;
+        const tower_id = 2;
+        const body = { name, cfn, fighter_id, tower_id };
+        const response = await request('http://localhost:8005').post('/player').send(body);
+        const errorMessages = JSON.parse(response.body.errors.message);
+        expect(response.status).toBe(400);
+        expect(errorMessages).toMatchObject([
+            {
+                code: 'invalid_type',
+                path: ['cfn'],
+                message: expect.stringContaining("O campo 'cfn' é obrigatório."),
+            },
+        ]);
+    });
+
+    it('should return 400 if fighter_id is not an integer', async () => {
+        const name = 'player1';
+        const cfn = "player bobao";
+        const fighter_id = "not_a_number";
+        const tower_id = 2;
+        const body = { name, cfn, fighter_id, tower_id };
+        const response = await request('http://localhost:8005').post('/player').send(body);
+        const errorMessages = JSON.parse(response.body.errors.message);
+        expect(response.status).toBe(400);
+        expect(errorMessages).toMatchObject([
+            {
+                code: 'invalid_type',
+                path: ['fighter_id'],
+                message: expect.stringContaining("O campo 'fighter_id' é obrigatório e deve ser um número inteiro"),
+            },
+        ]);
+    });
+
+    it('should return 400 if tower_id is not an integer', async () => {
+        const name = 'player1';
+        const cfn = "player bobao";
+        const fighter_id = 12355533;
+        const tower_id = "not_a_number";
+        const body = { name, cfn, fighter_id, tower_id };
+        const response = await request('http://localhost:8005').post('/player').send(body);
+        const errorMessages = JSON.parse(response.body.errors.message);
+        expect(response.status).toBe(400);
+        expect(errorMessages).toMatchObject([
+            {
+                code: 'invalid_type',
+                path: ['tower_id'],
+                message: expect.stringContaining("O campo 'tower_id' é obrigatório e deve ser um número inteiro."),
             },
         ]);
     });
@@ -102,52 +195,51 @@ describe('POST /player', () => {
         const fighter_id = 12355533;
         const tower_id = 2;
         const body = { name, cfn, fighter_id, tower_id };
-        await db('players').insert({ name, cfn, fighter_id, tower_id });
+        await db('player').insert({ name, cfn, fighter_id });
+        createdFighterIds.add(fighter_id);
         const response = await request('http://localhost:8005').post('/player').send(body);
         expect(response.status).toBe(409);
         expect(response.body).toEqual({ error: 'Fighter ID already exists' });
-        await db('players').where('fighter_id', fighter_id).del();
     });
 
     it('should return 200 and delete the player', async () => {
         const name = 'player1';
         const cfn = "player bobao";
         const fighter_id = 12355533;
-        const tower_id = 2;
-        await db('players').insert({ name, cfn, fighter_id, tower_id });
+        await db('player').insert({ name, cfn, fighter_id, deleted_at: null });
+        createdFighterIds.add(fighter_id);
         const response = await request('http://localhost:8005').delete(`/player/${fighter_id}`);
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({ message: 'Player deleted successfully' });
-        const count = await db('players').where('fighter_id', fighter_id).select().count();
-        expect(count[0]['count']).toBe('0');
+        expect(response.body).toEqual({ message: 'Jogador deletado com sucesso.' });
+        const player = await db('player').where('fighter_id', fighter_id).first();
+        expect(player.deleted_at).not.toBeNull();
     });
 
     it('should return 404 if player to delete is not found', async () => {
         const fighter_id = 99999999;
         const response = await request('http://localhost:8005').delete(`/player/${fighter_id}`);
         expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'Player not found' });
+        expect(response.body).toEqual({ error: 'Jogador não encontrado.' });
     });
 
     it('should return 200 and update the player', async () => {
         const name = 'player1';
         const cfn = "player bobao";
         const fighter_id = 12355533;
-        const tower_id = 2;
-        await db('players').insert({ name, cfn, fighter_id, tower_id });
+        await db('player').insert({ name, cfn, fighter_id });
+        createdFighterIds.add(fighter_id);
         const response = await request('http://localhost:8005').put(`/player/${fighter_id}`).send({ name: 'player1_updated' });
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({ message: 'Player updated successfully' });
-        const updatedPlayer = await db('players').where('fighter_id', fighter_id).first();
+        expect(response.body).toEqual({ name: 'player1_updated', cfn, fighter_id: fighter_id.toString() });
+        const updatedPlayer = await db('player').where('fighter_id', fighter_id).first();
         expect(updatedPlayer.name).toBe('player1_updated');
-        await db('players').where('fighter_id', fighter_id).del();
     });
 
     it('should return 404 if player to update is not found', async () => {
         const fighter_id = 99999999;
         const response = await request('http://localhost:8005').put(`/player/${fighter_id}`).send({ name: 'player1_updated' });
         expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'Player not found' });
+        expect(response.body).toEqual({ error: 'Jogador não encontrado.' });
     });
 
     it('should return 200 and list all players', async () => {
@@ -158,27 +250,24 @@ describe('POST /player', () => {
         expect(response.body[0]).toHaveProperty('name');
         expect(response.body[0]).toHaveProperty('cfn');
         expect(response.body[0]).toHaveProperty('fighter_id');
-        expect(response.body[0]).toHaveProperty('tower_id');
+        expect(response.body[0]).toHaveProperty('deleted_at');
     });
 
     it('should return 200 and get player by fighter_id', async () => {
         const name = 'player1';
         const cfn = "player bobao";
         const fighter_id = 12355533;
-        const tower_id = 2;
-        await db('players').insert({ name, cfn, fighter_id, tower_id });
+        await db('player').insert({ name, cfn, fighter_id });
+        createdFighterIds.add(fighter_id);
         const response = await request('http://localhost:8005').get(`/player/${fighter_id}`);
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({ name, cfn, fighter_id, tower_id });
-        await db('players').where('fighter_id', fighter_id).del();
+        expect(response.body).toEqual({ name, cfn, fighter_id:fighter_id.toString(),deleted_at: null });
     });
 
     it('should return 404 if player to get is not found', async () => {
         const fighter_id = 99999999;
         const response = await request('http://localhost:8005').get(`/player/${fighter_id}`);
         expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'Player not found' });
+        expect(response.body).toEqual({ error: 'Jogador não encontrado.' });
     });
-
-
 });
